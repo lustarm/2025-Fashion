12a3605e8bab41eb85fb61f841e41e1de7f5fdb6
jeweler <valve.valve101@gmail.com>
2024-12-21 15:47:45 -0500

    add home-images.json

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/nav";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+    // Grab these from backend etc
     const images = [
         { src: "./src/assets/SideRickOwens.jpg", alt: "Rick Owens Shoes", description: "Rick Owens Handmade Leather Resole", name: "rick-owens-handmade-leather-resole" },
         { src: "./src/assets/Hellstar-Powered-By-The-Star-L-S-Tee-Black.jpg", alt: "Hellstar Tee", description: "Powered by The Star L/S Tee", name: "hellstar-powered-by-the-star-tee" },
@@ -21,7 +22,7 @@ export default function Home() {
                         <Link
                             key={index}
                             to={`/shop/${image.name}`}
-                            className="relative block overflow-hidden bg-amber-100 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_5px_rgba(254,243,199,0.8)]"
+                            className="relative block overflow-hidden bg-zinc-500 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_5px_rgba(255,255,255,0.8)]"
                         >
                             <img
                                 src={image.src}
@@ -29,8 +30,8 @@ export default function Home() {
                                 className="w-full h-64 object-cover rounded-t-lg"
                             />
                             <div className="p-4">
-                                <p className="text-black font-semibold text-lg">{image.alt}</p>
-                                <p className="text-sm text-gray-400">{image.description}</p>
+                                <p className="text-white font-semibold text-lg">{image.alt}</p>
+                                <p className="text-sm text-zinc-300">{image.description}</p>
                             </div>
                         </Link>
                     ))}
